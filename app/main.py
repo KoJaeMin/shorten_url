4ce1ab79b5b113f384fdc70bf6314b53874bdbbb
Jae Min Ko <jvc123456@khu.ac.kr>
2024-06-04 00:54:50 +0900

    chore(path): add path from url

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from shorten.controller import shortenController
 from database.connect import engine
-from url.model import Base
+from url import Base
 
 Base.metadata.create_all(bind=engine)
 
